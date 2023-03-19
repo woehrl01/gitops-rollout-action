fca6560e2d335292ddde6733b87d09c322bda533
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2023-03-19 12:21:43 +0100

    format

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@
 /* eslint-disable no-console */
 /* eslint-disable prettier/prettier */
 import * as core from '@actions/core'
-import * as fs from 'fs';
+import * as fs from 'fs'
 import * as github from '@actions/github'
-import * as path from 'path';
-import glob from 'glob';
-import { minimatch } from 'minimatch';
+import * as path from 'path'
+import glob from 'glob'
+import {minimatch} from 'minimatch'
 
 async function run(): Promise<void> {
   try {
@@ -33,14 +33,14 @@ async function findIssueWithLabel(
   repo: string,
   label: string
 ): Promise<any> {
-  const { data: issues } = await octokit.rest.issues.listForRepo({
+  const {data: issues} = await octokit.rest.issues.listForRepo({
     owner,
     repo,
     labels: label,
-    state: 'open',
-  });
+    state: 'open'
+  })
 
-  return issues.length > 0 ? issues[0] : null;
+  return issues.length > 0 ? issues[0] : null
 }
 
 async function findOrCreateIssueWithLabel(
@@ -49,23 +49,25 @@ async function findOrCreateIssueWithLabel(
   repo: string,
   label: string
 ): Promise<any> {
-  const issue = await findIssueWithLabel(octokit, owner, repo, label);
+  const issue = await findIssueWithLabel(octokit, owner, repo, label)
 
   if (issue) {
-    return issue;
+    return issue
   }
 
-  console.log(`No open issue found with the label "${label}". Creating a new one...`);
+  console.log(
+    `No open issue found with the label "${label}". Creating a new one...`
+  )
 
-  const { data: newIssue } = await octokit.rest.issues.create({
+  const {data: newIssue} = await octokit.rest.issues.create({
     owner,
     repo,
     title: 'State Machine Issue',
     body: `<!-- STATE: ${JSON.stringify({})} -->`,
-    labels: [label],
-  });
+    labels: [label]
+  })
 
-  return newIssue;
+  return newIssue
 }
 
 const config: Config = {
@@ -74,76 +76,93 @@ const config: Config = {
       name: 'part1',
       filePattern: 'part1/**',
       target: 'generated/part1',
-      waitDurations: ["5m", "10m", "15m"]
-    },
+      waitDurations: ['5m', '10m', '15m']
+    }
   ]
 }
 
 interface Config {
-  parts: Part[];
+  parts: Part[]
 }
 
 interface Part {
-  name: string;
-  filePattern: string;
-  target: string;
+  name: string
+  filePattern: string
+  target: string
   waitDurations: string[]
 }
 
 interface State {
-  waitDurations: string[];
-  last_rollout_timestamp: number;
-  current_ring: number;
+  waitDurations: string[]
+  last_rollout_timestamp: number
+  current_ring: number
 }
 
 async function handlePush(): Promise<void> {
-  const token = core.getInput('repo-token', { required: true });
-  const octokit = github.getOctokit(token);
+  const token = core.getInput('repo-token', {required: true})
+  const octokit = github.getOctokit(token)
 
   //get all changed files of commit
-  const changedFiles = github.context.payload.commits[0].added.concat(github.context.payload.commits[0].modified);
+  const changedFiles = github.context.payload.commits[0].added.concat(
+    github.context.payload.commits[0].modified
+  )
 
   //get all parts that have changed files
-  const changedParts = config.parts.filter(part => changedFiles.some((file: string) => minimatch(file, part.filePattern)));
+  const changedParts = config.parts.filter(part =>
+    changedFiles.some((file: string) => minimatch(file, part.filePattern))
+  )
 
   //get all issues that have a label of a changed part
-  const issues = await Promise.all(changedParts.map(async part => findOrCreateIssueWithLabel(octokit, github.context.repo.owner, github.context.repo.repo, part.name)));
+  const issues = await Promise.all(
+    changedParts.map(async part =>
+      findOrCreateIssueWithLabel(
+        octokit,
+        github.context.repo.owner,
+        github.context.repo.repo,
+        part.name
+      )
+    )
+  )
 
   //find all parts without an open issue
-  const partsWithoutIssue = config.parts.filter(part => !issues.some(issue => issue.labels.some((label: { name: string }) => label.name === part.name)));
+  const partsWithoutIssue = config.parts.filter(
+    part =>
+      !issues.some(issue =>
+        issue.labels.some((label: {name: string}) => label.name === part.name)
+      )
+  )
 
   for (const part of partsWithoutIssue) {
     //create a new issue for the part
     const initalState = {
       number_of_rings: 0,
-      current_ring: 0,
+      current_ring: 0
     }
 
-    copyInitialFiles(part);
+    copyInitialFiles(part)
 
     await octokit.rest.issues.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       title: `Rollout ${part.name}`,
       body: `<!-- STATE: ${JSON.stringify(initalState)} -->`,
-      labels: [`part:${part.name}`],
-    });
+      labels: [`part:${part.name}`]
+    })
   }
 }
 
 async function copyInitialFiles(part: Part): Promise<void> {
-  const target = path.join(part.target, '0');
+  const target = path.join(part.target, '0')
 
   //copy all files from part.filePattern to part.target
-  const files = await getFiles(part.filePattern);
+  const files = await getFiles(part.filePattern)
 
   for (const file of files) {
+    const targetFile = path.join(target, path.basename(file))
 
-    const targetFile = path.join(target, path.basename(file));
+    console.log(`Copying ${file} to ${targetFile}`)
 
-    console.log(`Copying ${file} to ${targetFile}`);
-
-    fs.copyFileSync(file, targetFile);
+    fs.copyFileSync(file, targetFile)
   }
 }
 
@@ -151,43 +170,59 @@ async function getFiles(pattern: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
     glob(pattern, (err: any, files: string[]) => {
       if (err) {
-        reject(err);
+        reject(err)
       } else {
-        resolve(files);
+        resolve(files)
       }
-    });
-  });
+    })
+  })
 }
 
 async function handleSchedule(): Promise<void> {
-
   // Find all open issues of parts
-  const token = core.getInput('repo-token', { required: true });
-  const octokit = github.getOctokit(token);
+  const token = core.getInput('repo-token', {required: true})
+  const octokit = github.getOctokit(token)
 
-  const issues = await Promise.all(config.parts.map(async part => findOrCreateIssueWithLabel(octokit, github.context.repo.owner, github.context.repo.repo, part.name)));
+  const issues = await Promise.all(
+    config.parts.map(async part =>
+      findOrCreateIssueWithLabel(
+        octokit,
+        github.context.repo.owner,
+        github.context.repo.repo,
+        part.name
+      )
+    )
+  )
 
   // Iterate over all issues
   for (const issue of issues) {
-
     // Get the state from the issue body
-    const state = getStateFromBody(issue.body);
+    const state = getStateFromBody(issue.body)
 
     // Get the part for the issue
-    const part = config.parts.find(p => issue.labels.some((label: { name: string }) => label.name === p.name));
+    const part = config.parts.find(p =>
+      issue.labels.some((label: {name: string}) => label.name === p.name)
+    )
 
     if (!part) {
-      throw new Error(`Could not find part for issue ${issue.number}`);
+      throw new Error(`Could not find part for issue ${issue.number}`)
     }
 
-    const flags = getFlagsFromLabels(issue.labels);
+    const flags = getFlagsFromLabels(issue.labels)
 
     // Get the next state
-    const newState = await getNextState(state, part, flags);
+    const newState = await getNextState(state, part, flags)
 
     // Only update the issue if the state has changed
     if (JSON.stringify(newState) !== JSON.stringify(state)) {
-      await updateStateInBody(octokit, github.context.repo.owner, github.context.repo.repo, issue.number, newState, issue.labels);
+      await updateStateInBody(
+        octokit,
+        github.context.repo.owner,
+        github.context.repo.repo,
+        issue.number,
+        newState,
+        issue.labels
+      )
     }
 
     // Close the issue if the state is finished
@@ -197,80 +232,83 @@ async function handleSchedule(): Promise<void> {
         repo: github.context.repo.repo,
         issue_number: issue.number,
         state: 'closed'
-      });
+      })
     }
   }
 }
 
 interface FlagsFromLabels {
-  isFastlane: boolean;
-  isPaused: boolean;
-  isAborted: boolean;
+  isFastlane: boolean
+  isPaused: boolean
+  isAborted: boolean
 }
-
 
 function isShouldCloseIssue(state: State, flags: FlagsFromLabels): boolean {
   if (flags.isAborted) {
-    return true;
+    return true
   }
-  return state.current_ring >= state.waitDurations.length;
+  return state.current_ring >= state.waitDurations.length
 }
 
-function getFlagsFromLabels(labels: { name: string }[]): FlagsFromLabels {
+function getFlagsFromLabels(labels: {name: string}[]): FlagsFromLabels {
   return {
     isFastlane: labels.some(label => label.name === 'fastlane'),
     isPaused: labels.some(label => label.name === 'paused'),
-    isAborted: labels.some(label => label.name === 'abort'),
+    isAborted: labels.some(label => label.name === 'abort')
   }
 }
 
-async function getNextState(currentState: State, part: Part, flags: FlagsFromLabels): Promise<any> {
-
+async function getNextState(
+  currentState: State,
+  part: Part,
+  flags: FlagsFromLabels
+): Promise<any> {
   if (currentState.current_ring < currentState.waitDurations.length) {
-
     if (flags.isAborted) {
-      console.log('Rollout is aborted. Skipping...');
-      return currentState;
+      console.log('Rollout is aborted. Skipping...')
+      return currentState
     }
 
     if (flags.isPaused) {
-      console.log('Rollout is paused. Skipping...');
-      return currentState;
+      console.log('Rollout is paused. Skipping...')
+      return currentState
     }
 
     if (flags.isFastlane) {
-      console.log('Fastlane is enabled. increase ring...');
-      return increaseRing(currentState, part);
+      console.log('Fastlane is enabled. increase ring...')
+      return increaseRing(currentState, part)
     }
 
-    const waitDuration = currentState.waitDurations[currentState.current_ring];
-    const waitDurationInMs = parseGolangDuration(waitDuration);
-    const timeSinceLastRollout = Date.now() - currentState.last_rollout_timestamp;
+    const waitDuration = currentState.waitDurations[currentState.current_ring]
+    const waitDurationInMs = parseGolangDuration(waitDuration)
+    const timeSinceLastRollout =
+      Date.now() - currentState.last_rollout_timestamp
 
     if (timeSinceLastRollout < waitDurationInMs) {
-      console.log(`Not enough time has passed since last rollout. Wait for ${waitDuration} before rolling out to next ring.`);
-      return currentState;
+      console.log(
+        `Not enough time has passed since last rollout. Wait for ${waitDuration} before rolling out to next ring.`
+      )
+      return currentState
     }
 
-    return increaseRing(currentState, part);
+    return increaseRing(currentState, part)
   }
 
-  return currentState;
+  return currentState
 }
 
 async function increaseRing(currentState: State, part: Part): Promise<State> {
-
-  const currentRingLocation = `${part.target}/${currentState.current_ring}`;
-  const nextRingLocation = `${part.target}/${currentState.current_ring + 1}`;
+  const currentRingLocation = `${part.target}/${currentState.current_ring}`
+  const nextRingLocation = `${part.target}/${currentState.current_ring + 1}`
 
   // Copy files from current ring to next ring
-  await copyFolder(currentRingLocation, nextRingLocation);
+  await copyFolder(currentRingLocation, nextRingLocation)
 
   return {
     ...currentState,
     ...{
       last_rollout_timestamp: Date.now(),
-      current_ring: currentState.current_ring + 1,
+      current_ring: currentState.current_ring + 1
     }
   }
 }
@@ -278,32 +316,31 @@ async function increaseRing(currentState: State, part: Part): Promise<State> {
 async function copyFolder(src: string, dest: string): Promise<void> {
   // Create the destination directory if it doesn't exist
   if (!fs.existsSync(dest)) {
-    fs.mkdirSync(dest, { recursive: true });
+    fs.mkdirSync(dest, {recursive: true})
   }
 
   // Read the source directory
-  const entries = fs.readdirSync(src, { withFileTypes: true });
+  const entries = fs.readdirSync(src, {withFileTypes: true})
 
   // Iterate through the entries and handle files and directories separately
   for (const entry of entries) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
+    const srcPath = path.join(src, entry.name)
+    const destPath = path.join(dest, entry.name)
 
     if (entry.isDirectory()) {
       // If the entry is a directory, call the copyFolder function recursively
-      await copyFolder(srcPath, destPath);
+      await copyFolder(srcPath, destPath)
     } else {
       // If the entry is a file, copy the file
-      fs.copyFileSync(srcPath, destPath);
+      fs.copyFileSync(srcPath, destPath)
     }
   }
 }
 
-
 // Helper function to extract state from issue body
 function getStateFromBody(body: string): any {
-  const match = body.match(/<!-- STATE: (.*?) -->/);
-  return match ? JSON.parse(match[1]) : null;
+  const match = body.match(/<!-- STATE: (.*?) -->/)
+  return match ? JSON.parse(match[1]) : null
 }
 
 // Helper function to update state in issue body
@@ -313,62 +350,68 @@ async function updateStateInBody(
   repo: string,
   issueNumber: number,
   newState: State,
-  currentLabels: { name: string }[]
+  currentLabels: {name: string}[]
 ): Promise<void> {
-
   const issue = await findOrCreateIssueWithLabel(octokit, owner, repo, 'abc')
 
   const newBody = issue.body.replace(
     /<!-- STATE: (.*?) -->/,
     `<!-- STATE: ${JSON.stringify(newState)} -->`
-  );
+  )
 
-  const keepLabels = currentLabels.filter(label => !label.name.startsWith('ring:'));
+  const keepLabels = currentLabels.filter(
+    label => !label.name.startsWith('ring:')
+  )
 
   await octokit.rest.issues.update({
     owner,
     repo,
     issue_number: issueNumber,
     body: newBody,
-    labels: [`ring:${newState.current_ring}/${newState.waitDurations.length}`, ...keepLabels],
-  });
+    labels: [
+      `ring:${newState.current_ring}/${newState.waitDurations.length}`,
+      ...keepLabels
+    ]
+  })
 }
 
 function parseGolangDuration(durationStr: string): number {
-  const durationRegex = /(\d+(\.\d+)?)([a-z]+)/g;
-  let totalMilliseconds = 0;
+  const durationRegex = /(\d+(\.\d+)?)([a-z]+)/g
+  let totalMilliseconds = 0
 
-  let match: RegExpExecArray | null;
+  let match: RegExpExecArray | null
 
   while ((match = durationRegex.exec(durationStr)) !== null) {
-    const value = parseFloat(match[1]);
-    const unit = match[3];
+    const value = parseFloat(match[1])
+    const unit = match[3]
 
     switch (unit) {
       case 'ns':
-        totalMilliseconds += value * 1e-6;
-        break;
+        totalMilliseconds += value * 1e-6
+        break
       case 'us':
-        totalMilliseconds += value * 0.001;
-        break;
+        totalMilliseconds += value * 0.001
+        break
       case 'ms':
-        totalMilliseconds += value;
-        break;
+        totalMilliseconds += value
+        break
       case 's':
-        totalMilliseconds += value * 1000;
-        break;
+        totalMilliseconds += value * 1000
+        break
       case 'm':
-        totalMilliseconds += value * 1000 * 60;
-        break;
+        totalMilliseconds += value * 1000 * 60
+        break
       case 'h':
-        totalMilliseconds += value * 1000 * 60 * 60;
-        break;
+        totalMilliseconds += value * 1000 * 60 * 60
+        break
       default:
-        throw new Error(`Unknown unit "${unit}" in duration string "${durationStr}"`);
+        throw new Error(
+          `Unknown unit "${unit}" in duration string "${durationStr}"`
+        )
     }
   }
 
-  return totalMilliseconds;
+  return totalMilliseconds
 }
 
 run()
